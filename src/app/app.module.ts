3147866db0d659d3518148ee22290d51503dc677
feliperomao-dev <feliperomaodev@outlook.com>
2022-09-25 20:01:24 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course/course-list.component';
+import { StarComponent } from './star/star.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SubtituloComponent } from './subtitulo/subtitulo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    SubtituloComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
